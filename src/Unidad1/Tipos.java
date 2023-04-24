8d6267d71840659f80c5f0be77ffae552faedea8
Kalalo7 <martinfauci@gmail.com>
2023-04-24 18:11:59 +0200

    estructura de carpetas

---
+++ src/Unidad1/Tipos.java
@@ -0,0 +1,35 @@
+package Unidad1;
+
+public class Tipos {
+    public static void main(String[] args) {
+        byte byteVar = 127;
+        System.out.println("Tipo byte: " + byteVar);
+
+        short shortVar = 32767;
+        System.out.println("Tipo short: " + shortVar);
+
+        int intVar = 2147483647;
+        System.out.println("Tipo int: " + intVar);
+
+        long longVar = 9223372036854775807L;
+        System.out.println("Tipo long: " + longVar);
+
+        float floatVar = 3.14f;
+        System.out.println("Tipo float: " + floatVar);
+
+        double doubleVar = 3.141592653589793;
+        System.out.println("Tipo double: " + doubleVar);
+
+        boolean booleanVar = true;
+        System.out.println("Tipo boolean: " + booleanVar);
+
+        boolean booleanVar2 = false;
+        System.out.println("Tipo boolean: " + booleanVar2);
+
+        char charVar = 'M';
+        System.out.println("Tipo char: " + charVar);
+
+        String stringVar = "Hola mundo!";
+        System.out.println("Tipo String: " + stringVar);
+    }
+}
